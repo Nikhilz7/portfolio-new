9a5178a2d8c086e0dedab3bb783704044e2122f7
Nikhil <nikhilc71@gmail.com>
2023-07-06 23:06:59 +0530

    all links added

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,9 +1,14 @@
-import { AiOutlineLinkedin } from "react-icons/ai";
+import { BsLinkedin } from "react-icons/bs";
 import {CgMail} from "react-icons/cg"
+import {MdContactPage} from "react-icons/md"
 import {FaGithubSquare} from "react-icons/fa"
 
 
 const Contact = () => {
+    const mail:string =import.meta.env.VITE_MAIL;
+    const linkedin:string =import.meta.env.VITE_LINKEDIN;
+    const github:string =import.meta.env.VITE_GITHUB;
+    const resume:string =import.meta.env.VITE_RESUME;
 
     return (
         <div id="contact" className="">
@@ -13,20 +18,23 @@ const Contact = () => {
             <h1 className="sm:text-6xl text-4xl font-display font-bold bg-neutral-200 bg-clip-text text-transparent ">Connect with Me</h1>
                 <p className="mt-6 sm:text-2xl font-display bg-neutral-200 bg-clip-text text-transparent">As a passionate web developer, I thrive on creating exceptional online experiences that captivate and engage users.Whether you need a stunning website, a dynamic web application, or assistance with any aspect of web development. My expertise includes front-end development, back-end programming, responsive design, and more. I am committed to delivering high-quality, visually appealing, and user-friendly solutions tailored to your unique requirements.</p>
 
-            <div className="mt-4 flex flex-row ">
-            <a href="" className="flex items-center m-3">
-                <AiOutlineLinkedin color="white" size={50}/>
-                <p className="sm:text-2xl text-md font-display font-bold bg-neutral-200 bg-clip-text text-transparent">LinkedIn</p>
-            </a>
-            <a href="" className="flex items-center m-3">
-                <CgMail color="white" size={50}/>
-                <p className="sm:text-2xl text-md font-display font-bold bg-neutral-200 bg-clip-text text-transparent">Mail</p>
-            </a>
-            <a href="" className="flex items-center m-3">
-                <FaGithubSquare color="white" size={50}/>
-                <p className="sm:text-2xl text-md font-display font-bold bg-neutral-200 bg-clip-text text-transparent">Github</p>
-            </a>
-            
+            <div className="flex flex-row justify-between pt-6 ">
+                <span onClick={()=>window.open(linkedin,'_blank')?.focus()} className="flex items-center m-3 hover:cursor-pointer">
+                    <BsLinkedin color="white" size={50}/>
+                    <p className="sm:text-2xl text-md font-display font-bold bg-neutral-200 bg-clip-text text-transparent">LinkedIn</p>
+                </span>
+                <span  onClick={()=>window.open('mailto:'+mail,'_blank')?.focus()}  className="flex items-center m-3 hover:cursor-pointer">
+                    <CgMail color="white" size={50}/>
+                    <p className="sm:text-2xl text-md font-display font-bold bg-neutral-200 bg-clip-text text-transparent">Mail</p>
+                </span>
+                <span onClick={()=>window.open(github,'_blank')?.focus()} className="flex items-center m-3 hover:cursor-pointer">
+                    <FaGithubSquare color="white" size={50}/>
+                    <p className="sm:text-2xl text-md font-display font-bold bg-neutral-200 bg-clip-text text-transparent">Github</p>
+                </span>
+                <span onClick={()=>window.open(resume,'_blank')?.focus()} className="flex items-center m-3 hover:cursor-pointer">
+                    <MdContactPage color="white" size={50}/>
+                    <p className="sm:text-2xl text-md font-display font-bold bg-neutral-200 bg-clip-text text-transparent">Resume</p>
+                </span>
             </div>
             
         </div>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -14,26 +14,26 @@ const Projects = () => {
     
     <div id="projects" className="relative w-[600] h-full bg-gradient-to-br from-indigo-800 via-neutral-900 to-indigo-900 flex flex-row justify-center ">
         <div className="sm:max-w-[1700px] max-w-[900px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-start sm:ml-28">
-            <h1 className="sm:text-6xl text-4xl font-display font-bold bg-neutral-200 bg-clip-text text-transparent sm:ml-[600px]">Projects</h1>
+            <h1 className="sm:text-6xl text-4xl font-display font-bold bg-neutral-200 bg-clip-text text-transparent sm:ml-[700px] mb-6">Projects</h1>
             
                 {projects.map((project)=>(
-              <div className="sm:items-center flex sm:flex-row flex-col p-2 m-6">
-                  <div className="sm:w-full sm:min-h-[300px] bg-blend-lighten md:bg-blend-darken shadow-[0px_0px_30px_white] rounded-3xl   ">
-                  
-                        <img src={project.image} alt="" className=" sm:w-[1200px] sm:h-[400px] object-fill hover:translate-x-1 ease-out rounded-3xl sm:opacity-90 hover:opacity-100"/>
-                    </div>  
-                  <div className="w-full max-w-5xl relative pl-16 flex flex-col ">
-                    <div className="pt-4 font-display bg-neutral-200 bg-clip-text text-transparent">
-                        <h3 className="sm:text-4xl text-2xl text-transparent  text-neutral-100">
-                            {project.name}
-                        </h3>
-                        <p className="sm:text-2xl text-sm pt-8">Made with: {project.tech}</p>
-                        <p className="sm:text-lg text-m p-3">{project.description}</p>
-                        <Button value="Live" disabled={false} link={project.live_link as string}/>
-                        <Button value="Github" disabled={false} link={project.github_link}/>
+                    <div className="sm:items-center flex sm:flex-row flex-col p-2 m-6">
+                        <div className="sm:w-full sm:min-h-[300px] bg-blend-lighten md:bg-blend-darken shadow-[0px_0px_30px_white] hover:scale-110 ease-in duration-200 rounded-3xl   ">
+                        
+                                <img src={project.image} alt="" className=" sm:w-[1200px] sm:h-[400px] object-fill  rounded-3xl sm:opacity-90 hover:opacity-100"/>
+                            </div>  
+                        <div className="w-full max-w-5xl relative pl-16 flex flex-col ">
+                            <div className="pt-4 font-display bg-neutral-200 bg-clip-text text-transparent">
+                                <h3 className="sm:text-4xl text-2xl text-transparent  text-neutral-100">
+                                    {project.name}
+                                </h3>
+                                <p className="sm:text-2xl text-sm pt-8">Made with: {project.tech}</p>
+                                <p className="sm:text-lg text-m p-3">{project.description}</p>
+                                <Button value="Live" disabled={false} link={project.live_link as string}/>
+                                <Button value="Github" disabled={false} link={project.github_link}/>
+                                </div>
+                            </div>  
                         </div>
-                    </div>  
-                </div>
                 ))}
                 </div>
       </div>
